c0647b71c169600794ab8380db5e4fc8bdfd3ecb
shane <shanecandoit@gmail.com>
2020-01-21 11:20:12 -0600

    passes many cases. fail on indentical test.

--- anagram.go
+++ anagram.go
@@ -1,10 +1,53 @@
 package anagram
 
+// package main
+
+import (
+	"sort"
+	"strings"
+)
+
 // Detect possible anagrams from the subject to possible candidates
 func Detect(subject string, candidates []string) (anagrams []string) {
 
 	// Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana"
 	// the program should return a list containing "inlets".
+	// tip: anagrams are identical when sorted
 
-	return candidates
+	// fmt.Println("Detect:", subject, candidates)
+	subject = strings.ToLower(subject)
+
+	sortedSubject := SortString(subject)
+	// fmt.Println("sorted_subject", sorted_subject)
+
+	anagramCands := []string{}
+	for _, s := range candidates {
+
+		if len(s) != len(subject) {
+			// fmt.Println("bad_len", s)
+			continue
+		}
+
+		sLow := strings.ToLower(s)
+		sortCand := SortString(sLow)
+		// fmt.Println("sort_cand", sort_cand)
+		if sortCand == sortedSubject {
+			unsortedCand := s
+			anagramCands = append(anagramCands, unsortedCand)
+		}
+	}
+
+	return anagramCands
+}
+
+// SortString break, sort, join is wastefull
+func SortString(w string) string {
+	// https://stackoverflow.com/questions/22688651/golang-how-to-sort-string-or-byte
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+
+	// this fails: panic: reflect: call of Swapper on zero Value
+	// sort.Slice(w, func(i int, j int) bool { return w[i] < w[j] })
+	// return string(w)
 }
