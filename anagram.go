5c7e0b554facd4a86b7eafa7c4b5886ff11927c1
shane <shanecandoit@gmail.com>
2020-01-21 10:55:21 -0600

    fail tests. function prototpye

--- anagram.go
+++ anagram.go
@@ -1,1 +1,10 @@
 package anagram
+
+// Detect possible anagrams from the subject to possible candidates
+func Detect(subject string, candidates []string) (anagrams []string) {
+
+	// Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana"
+	// the program should return a list containing "inlets".
+
+	return candidates
+}
